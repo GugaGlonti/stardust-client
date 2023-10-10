259494dfe74895850c4f163c501d377881e1d12e
Guga Glonti <110097877+GugaGlonti@users.noreply.github.com>
2023-10-10 17:50:54 +0200

    fixed? authprovider warning

--- src/Pages/Layouts/RootLayout.tsx
+++ src/Pages/Layouts/RootLayout.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { LoaderFunction, Outlet, useLoaderData } from 'react-router';
 
 import AuthService from '../../services/auth.service';
@@ -12,11 +12,17 @@ import { authContext } from '../../store/auth.provider';
 import NotificationService from '../../services/notification.service';
 
 export default function RootLayout() {
+  const user = useLoaderData() as User;
+
   const context = useContext(authContext);
   const { setLoggedInUser: setUser } = context;
 
-  const user = useLoaderData() as User;
-  if (user) setUser && setUser(user);
+  useEffect(() => {
+    (async () => {
+      if (!user) return;
+      setUser && setUser(user);
+    })();
+  });
 
   return (
     <>
@@ -33,9 +39,7 @@ export const loadProfileData: LoaderFunction = async () => {
   const response = await Promise.all([AuthService.me(), NotificationService.getMyNotificationCount()]);
   const me = { ...response[0], notificationCount: response[1] };
 
-  if (!me) {
-    return null;
-  }
+  if (!me) return null;
 
   return me;
 };
