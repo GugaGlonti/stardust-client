493f1e6f9f4152f41dc38177d5f6a0e729775f8a
Guga Glonti <110097877+GugaGlonti@users.noreply.github.com>
2023-10-15 21:27:25 +0200

    add colorbar to footer

--- src/modules/Footer/Footer.tsx
+++ src/modules/Footer/Footer.tsx
@@ -1,26 +1,31 @@
+import ColorBar from '../../components/UTIL/ColorBar';
+
 export default function Footer() {
   return (
-    <div className='flex justify-around p-32 bg-gray-800 text-gray-500'>
-      <div className='flex flex-col'>
-        <h1>something</h1>
-        <h1>something</h1>
-        <h1>something</h1>
+    <>
+      <div className='flex justify-around p-32 bg-gray-800 text-gray-500'>
+        <div className='flex flex-col'>
+          <h1>something</h1>
+          <h1>something</h1>
+          <h1>something</h1>
+        </div>
+        <div className='flex flex-col'>
+          <h1>something</h1>
+          <h1>something</h1>
+          <h1>something</h1>
+        </div>
+        <div className='flex flex-col'>
+          <h1>something</h1>
+          <h1>something</h1>
+          <h1>something</h1>
+        </div>
+        <div className='flex flex-col'>
+          <h1>something</h1>
+          <h1>something</h1>
+          <h1>something</h1>
+        </div>
       </div>
-      <div className='flex flex-col'>
-        <h1>something</h1>
-        <h1>something</h1>
-        <h1>something</h1>
-      </div>
-      <div className='flex flex-col'>
-        <h1>something</h1>
-        <h1>something</h1>
-        <h1>something</h1>
-      </div>
-      <div className='flex flex-col'>
-        <h1>something</h1>
-        <h1>something</h1>
-        <h1>something</h1>
-      </div>
-    </div>
+      <ColorBar />
+    </>
   );
 }
