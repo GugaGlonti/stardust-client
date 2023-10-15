a6b815bfe7d542f187af022c15cd2f65fcb52410
Guga Glonti <110097877+GugaGlonti@users.noreply.github.com>
2023-10-15 21:41:17 +0200

    add margin to footer

--- src/modules/Footer/Footer.tsx
+++ src/modules/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import ColorBar from '../../components/UTIL/ColorBar';
 export default function Footer() {
   return (
     <>
-      <div className='flex justify-around p-32 bg-gray-800 text-gray-500'>
+      <div className='flex justify-around p-32 mt-32 bg-gray-700 text-gray-500'>
         <div className='flex flex-col'>
           <h1>something</h1>
           <h1>something</h1>
